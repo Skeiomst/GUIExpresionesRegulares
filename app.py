c5b9112fb95c8bf935621813fa75bb22d4044f1a
Skeiomst <122640508+Skeiomst@users.noreply.github.com>
2025-01-27 21:09:43 -0600

    Modificación ER en correo

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def validate_field():
     value = data.get('value')
     
     # Simulación de validación para cada campo
-    regex_patterns = {
+    patrones_validados = {
         "telefono": r"^\d{10}$",
-        "correo": r"^[\w\.-]+@[\w\.-]+\.\w+$",
+        "correo": r"^[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,4}$",
         "curp": r"^[A-Z]{1}[AEIOU]{1}[A-Z]{2}\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])[HM]{1}[A-Z]{2}[BCDFGHJKLMNÑPQRSTVWXYZ]{3}[0-9A-Z]{2}$",
         "rfc": r"(^[A-Z]{4}\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])[A-Z0-9]{3}$)|(^[A-Z]{3}\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])[A-Z0-9]{3}$)",
         "ipv4": r"^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$",
@@ -24,7 +24,7 @@ def validate_field():
     }
 
     import re
-    valid = bool(re.match(regex_patterns.get(field_name, ""), value))
+    valid = bool(re.match(patrones_validados.get(field_name, ""), value))
 
     return jsonify({"valid": valid})
 
